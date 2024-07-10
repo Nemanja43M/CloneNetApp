5a2afc916825c1c8e0f70900b62ea1530933d9ca
Nemanja43M <nemanja.markovic@mvsoft.rs>
2024-07-11 00:27:03 +0200

    Adding Header component with animation and implementing Routing

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./header.css";
+import img from "../assets/images/united_cloud_logo_1_.svg";
+
+const Header: React.FC = () => {
+    const [Mobile, setMobile] = useState<boolean>(false);
+    return (
+        <>
+            <header>
+                <div className="container flexSB">
+                    <nav className="flexSB">
+                        <div className="logo">
+                            <img className="logo-img" src={img} alt="" />
+                        </div>
+                        <ul className={Mobile ? "navMenu-list" : "flexSB"} onClick={() => setMobile(false)}>
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="/">Series</a>
+                            </li>
+                            <li>
+                                <a href="/">Movies</a>
+                            </li>
+                            <li>
+                                <a href="/">Pages</a>
+                            </li>
+                            <li>
+                                <a href="/">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="/">Contact</a>
+                            </li>
+                        </ul>
+                        <button className="toggle" onClick={() => setMobile(!Mobile)}>
+                            {Mobile ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
+                        </button>
+                    </nav>
+                    <div className="account flexSB">
+                        <i className="fa fa-search"></i>
+                        <i className="fas fa-bell"></i>
+                        <i className="fas fa-user"></i>
+                        <button>Subscribe Now</button>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
