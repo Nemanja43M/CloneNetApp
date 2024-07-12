2b690f6247f6c0a73e425a7d797d2b658da79030
Nemanja43M <nemanja.markovic@mvsoft.rs>
2024-07-12 09:00:38 +0200

    Minor ajustments

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -13,11 +13,11 @@ const Footer: React.FC = () => {
                             <li>Blog</li>
                         </ul>
                         <p>
-                            © 2024 United Cloud. All Rights Reserved. All videos
-                            and shows on this platform are trademarks of, and
-                            all related images and content are the property of,
-                            United Cloud Inc. Duplication and copy of this is
-                            strictly prohibited. All rights reserved.
+                            © 2024 United Cloud. All Rights Reserved. All
+                            videos and shows on this platform are trademarks of,
+                            and all related images and content are the property
+                            of, United Cloud Inc. Duplication and copy of this
+                            is strictly prohibited. All rights reserved.
                         </p>
                     </div>
                     <div className="box">
